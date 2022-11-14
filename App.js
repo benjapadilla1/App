af738a7eb93ec2ad494b0b9d421edc0481a3799b
Benjamin Padilla <benja.padilla@outlook.com.ar>
2022-11-13 23:42:34 -0300

    agregando modal

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Button, Text, TextInput, View, FlatList, Pressable, TouchableOpacity, Alert } from 'react-native';
+import { useState } from 'react';
+import Modal from './components/Modal';
 export default function App() {
+  const [textItem, setTextItem] = useState("")
+  const [list, setList] = useState([])
+  const [modalVisible, setModalVisible] = useState(false)
+  const [itemSelected, setitemSelected] = useState({})
+
+  const onHandleChange = (t) => setTextItem(t)
+  const addItem = () => {
+    setList(currenState => [
+      ...currenState,
+      { id: Math.random().toString(), value: textItem }
+    ])
+    setTextItem("")
+  }
+
+  const selectedItem = (id) => {
+    setitemSelected(list.filter((item) => item.id === id)[0])
+    setModalVisible(true)
+  }
+  const deleteItem = () => {
+    setList((currenState) => currenState.filter(item => item.id !== itemSelected.id))
+    setitemSelected({})
+    setModalVisible(false)
+  }
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => selectedItem(item.id)}>
+      <Text>{item.value}</Text>
+    </TouchableOpacity>
+  )
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text>Lista de shopping</Text>
+      <View style={styles.addItem}>
+        <TextInput
+          value={textItem}
+          style={styles.input}
+          placeholder="Agregar item a la lista"
+          onChangeText={onHandleChange}
+        />
+        <TouchableOpacity onPress={addItem}>
+          <Text style={styles.addButton}> Add </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+      <Modal isVisible={modalVisible} actionDeleteItem={deleteItem} style={styles.deleteItem} />
+    </View >
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#536976",
+    padding: 30,
+    margin: "0px auto",
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
   },
-});
+  input: {
+    width: 200,
+    borderBottomColor: "black",
+    borderBottomWidth: 1
+  },
+  addItem: {
+    marginTop: 50,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  items: {
+    marginTop: 50,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  deleteItem: {
+    marginTop: 30,
+  },
+  addButton: {
+    backgroundColor: "#FAA618",
+    borderRadius: "15px",
+  },
+})
